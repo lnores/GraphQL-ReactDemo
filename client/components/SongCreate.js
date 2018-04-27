6803bde9b7b54b70ce1fcb6a759d79201b7c2c16
Luis Nores <LNores@ccrlatam.com>
2018-04-27 09:48:01 -0500

    first commit with the complete code

---
+++ client/components/SongCreate.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
+
+class SongCreate extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { title: '' };
+    }
+
+    onSubmit() {
+        event.preventDefault();
+
+        this.props.mutate({
+            // the prop name is variables remember!!!
+            variables: { title: this.state.title },
+            // to reload the new data when we create a new document we should be use
+            refetchQueries: [{ query: query }] // or refetchQueries: [{ query }]
+        }).then(() => hashHistory.push('/'));
+    }
+
+    render() {
+        return (
+            <div>
+                <Link to="/">Back</Link>
+                <h3>Create a New Song</h3>
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <label>Song Title:</label>
+                    <input
+                        onChange={event => this.setState({ title: event.target.value })}
+                        value={this.state.title}
+                    />
+                </form>
+            </div>
+        );
+    }
+}
+
+const mutation = gql`
+    mutation AddSong($title: String) {
+        addSong(title: $title) {
+        title
+        }
+    }
+`;
+
+export default graphql(mutation)(SongCreate);
---
+++ client/index.js
@@ -0,0 +1,37 @@
+import './style/style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Routing
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+// Libraries to use graphQL with react
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+import App from './components/App';
+import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
+import SongDetail from './components/SongDetail';
+
+const client = new ApolloClient({
+  // More performance way to fetch data from the server is by use
+  dataIdFromObject: o => o.id
+});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+          <Route path="songs/new" component={SongCreate} />
+          <Route path="songs/:id" component={SongDetail} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(
+  <Root />,
+  document.querySelector('#root')
+);
